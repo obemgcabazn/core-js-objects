784eb271a03a0bb0009468ca6240e6dee39b7266
Electropompa <admin@zilmet.ru>
2025-03-05 12:46:59 +0300

    feat: compareObects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -82,8 +82,14 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const entries1 = Object.entries(obj1);
+  const entries2 = Object.entries(obj2);
+  if (entries1.length !== entries2.length) return false;
+  for (let i = 0; i < entries1.length; i += 1) {
+    if (entries1[i][1] !== entries2[i][1]) return false;
+  }
+  return true;
 }
 
 /**
