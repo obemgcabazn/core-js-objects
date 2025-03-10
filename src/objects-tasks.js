749f909decc87ba375ec9de7445741810aac294d
Electropompa <admin@zilmet.ru>
2025-03-10 12:47:55 +0300

    feat: fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -224,8 +224,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const newObj = {};
+  Object.setPrototypeOf(newObj, proto);
+  Object.assign(newObj, JSON.parse(json));
+  return newObj;
 }
 
 /**
